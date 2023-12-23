4fe28083e34abd9b540d16ab119f5d1f849ef911
Luiz Felipe Carminatti <felipecarminatti28@gmail.com>
2023-12-23 18:51:20 -0300

    Objeto

---
+++ Objeto/OO.js
@@ -0,0 +1,5 @@
+// Principios importantes:
+// 1: abstracao é a forma de pegar caracteristicas de um objeto da vida real para dentro do programa
+// 2: encapsulamento é esconder carateristicas de um objeto menos importantes para o usuario
+// 3: herança é um objeto herdar um caracteristica de outro objeto
+// 4: polimorfismo multiplas formas
