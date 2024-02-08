984e868b18046f9495ce3444e4c5a0fe696354ea
Luiz Felipe Carminatti <felipecarminatti28@gmail.com>
2024-02-08 13:57:53 -0300

    Atualização2

---
+++ gulp/basico/gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp')
+const { series, parallel } = require('gulp')
+
+function copiar(cb) {
+    /* gulp.src(['pastaA/arquivo1.txt', 'pastaB/arquivo2.txt']) */
+    gulp.src('pastaA/**/*.txt')
+        .pipe(gulp.dest('pastaB'))
+    return cb()
+}
+
+module.exports.default = series(copiar)
---
+++ gulp/javascript/gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp')
+const { series } = require('gulp')
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+
+function padrao(cb) {
+    
+    gulp.src('src/**/*.js')
+        .pipe(babel({
+            comments: false,
+            presets: ["env"] /* converte para uma versao mais nova do javascript */
+        }))
+        .pipe(uglify())
+        .pipe(concat('codigo.min.js'))
+        .pipe(gulp.dest('build'))
+
+    return cb()
+}
+
+module.exports.default = series(padrao)
