328df0badebbcb3fb70cb6604996c82e6cd8fa3e
Luiz Felipe Carminatti <felipecarminatti28@gmail.com>
2024-02-13 13:19:22 -0300

    Gulp

--- gulp/spa/gulpTasks/deps.js
+++ gulp/spa/gulpTasks/deps.js
@@ -3,7 +3,7 @@ const uglifycss = require('gulp-uglifycss')
 const concat = require('gulp-concat')
 
 function depsCSS() {
-    return gulp.src('node_modules\font-awesome\css\font-awesome.css')
+    return gulp.src('node_modules\font-awesome\css\font-awesome.css', { allowEmpty: true })
         .pipe(uglifycss({ "uglyComments": false }))
         .pipe(concat('deps.min.css'))
         .pipe(gulp.dest('build/assets/css'))
